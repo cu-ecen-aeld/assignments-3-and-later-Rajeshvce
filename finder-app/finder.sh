8ed6c5e0c01ef5975f40b23c830675a85965eb84
Rajeshvce <94607772+Rajeshvce@users.noreply.github.com>
2023-06-27 18:31:43 +0530

    Update finder.sh
    finder.sh changed for assignment-4

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,27 +1,22 @@
 #!/bin/sh
 
-# Check if all required arguments are provided
+
 if [ $# -ne 2 ]; then
-    echo "Error: Two arguments are required."
-    exit 1
+	echo "Usage: $0 <File_Directory> <Search_String>"
+	exit 1
 fi
 
-# Extract the arguments
-filesdir="$1"
-searchstr="$2"
+FILES_DIR=$1
+SEARCHING_STR=$2
 
-# Check if filesdir is a directory
-if [ ! -d "$filesdir" ]; then
-    echo "Error: $filesdir does not represent a directory on the filesystem."
-    exit 1
+if [ ! -d "$FILES_DIR" ]
+then
+	echo "Error : $FILES_DIR is not a directory"
+	exit 1
 fi
 
-# Search for matching lines in the files
-matching_lines=$(grep -r "$searchstr" "$filesdir" | wc -l)
+NO_OF_FILES=$(find "$FILES_DIR" -type f | wc -l)
+MATCHES_COUNT=$(grep -r "$SEARCHING_STR" "$FILES_DIR" | wc -l)
 
-# Count the number of files in the directory and all subdirectories
-num_files=$(find "$filesdir" -type f | wc -l)
-
-# Print the result
-echo "The number of files are $num_files and the number of matching lines are $matching_lines"
-
+echo "The number of files are $NO_OF_FILES and the number of matching lines are $MATCHES_COUNT" 
+ 
